7fd33a68891fd3c63503f4482e3676738edbc557
Travis Toal <travisty12@gmail.com>
2020-01-11 22:03:50 -0800

    add partials for forms, add css

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # root to: '/albums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :albums do
     resources :songs
   end
+  resources :home, only: [:index]
+  root to: 'home#index'
 end
